33f00ea5b5187bc3235533aa0e8ddedbcbcc2c70
Professor Linux <123519116+professor-linux@users.noreply.github.com>
2024-07-09 05:26:16 -0400

    Create array_functions.h

---
+++ array_functions.h
@@ -0,0 +1,11 @@
+#ifndef ARRAY_FUNCTIONS_H
+#define ARRAY_FUNCTIONS_H
+
+// Function prototypes
+int sum(int collection[], int size);
+double average(int collection[], int size);
+int findMax(int collection[], int size);
+int findMin(int collection[], int size);
+int printAll(int collection[], int size);
+void sort(int collection[], int size);
+#endif /* ARRAY_FUNCTIONS_H */
